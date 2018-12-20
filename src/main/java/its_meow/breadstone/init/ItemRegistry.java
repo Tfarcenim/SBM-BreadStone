87215554dbecae5814d84139ea3a09178be1a62f
BuildTools <mods.itsmeow@gmail.com>
2018-12-19 23:59:56 -0500

    Add armor

--- src/main/java/its_meow/breadstone/init/ItemRegistry.java
+++ src/main/java/its_meow/breadstone/init/ItemRegistry.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 import its_meow.breadstone.common.item.ItemStaleBread;
+import its_meow.breadstone.common.item.ItemStaleBreadArmor;
 import its_meow.breadstone.common.item.ItemStaleBreadAxe;
 import its_meow.breadstone.common.item.ItemStaleBreadHoe;
 import its_meow.breadstone.common.item.ItemStaleBreadPickaxe;
 import its_meow.breadstone.common.item.ItemStaleBreadShovel;
 import its_meow.breadstone.common.item.ItemStaleBreadSword;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
@@ -22,11 +24,19 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ItemRegistry {
 	
 	public static ItemStaleBread stalebread = new ItemStaleBread();
+	
+	// Tools
 	public static ItemStaleBreadSword stalebreadsword = new ItemStaleBreadSword();
 	public static ItemStaleBreadPickaxe stalebreadpickaxe = new ItemStaleBreadPickaxe();
 	public static ItemStaleBreadAxe stalebreadaxe = new ItemStaleBreadAxe();
 	public static ItemStaleBreadShovel stalebreadshovel = new ItemStaleBreadShovel();
 	public static ItemStaleBreadHoe stalebreadhoe = new ItemStaleBreadHoe();
+	
+	// Armor
+	public static ItemStaleBreadArmor stalebreadhelmet = new ItemStaleBreadArmor(EntityEquipmentSlot.HEAD, "helmet");
+	public static ItemStaleBreadArmor stalebreadchestplate = new ItemStaleBreadArmor(EntityEquipmentSlot.CHEST, "chestplate");
+	public static ItemStaleBreadArmor stalebreadleggings = new ItemStaleBreadArmor(EntityEquipmentSlot.LEGS, "leggings");
+	public static ItemStaleBreadArmor stalebreadboots = new ItemStaleBreadArmor(EntityEquipmentSlot.FEET, "helmet");
 	
 	@Mod.EventBusSubscriber
 	public static class RegistrationHandler {
@@ -45,7 +55,11 @@ public class ItemRegistry {
 					stalebreadpickaxe,
 					stalebreadaxe,
 					stalebreadshovel,
-					stalebreadhoe
+					stalebreadhoe,
+					stalebreadhelmet,
+					stalebreadchestplate,
+					stalebreadleggings,
+					stalebreadboots
 			};
 			final IForgeRegistry<Item> registry = event.getRegistry();
 
@@ -58,11 +72,17 @@ public class ItemRegistry {
 		@SubscribeEvent
 		public static void registerItemBlockModels(final ModelRegistryEvent event) {
 			initModel(stalebread, 0);
+			// Tools
 			initModel(stalebreadsword, 0);
 			initModel(stalebreadpickaxe, 0);
 			initModel(stalebreadaxe, 0);
 			initModel(stalebreadshovel, 0);
 			initModel(stalebreadhoe, 0);
+			// Armor
+			initModel(stalebreadhelmet, 0);
+			initModel(stalebreadchestplate, 0);
+			initModel(stalebreadleggings, 0);
+			initModel(stalebreadboots, 0);
 		}
 
 
